3a1ead311700492f830877af75e797080a36069c
Arcyy_Madlos <96642128+RyzenDev@users.noreply.github.com>
2023-04-25 14:11:45 +0800

    addedd basic chasacter controller

---
+++ characters/player.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+
+# always running right
+# junmp
+# crouch + gravity
+
+# kill function
+# slowing after hit
+# timer for speed gain
+# invulnerability
+
+# animations
+
+# extras
+# coyote + jump buffer + variable jump hiegth
+# particls
+# raycast for tile detection
+
+var speed = 200
+var jump_force = 200
+var crouch_force = 200
+var gravity = 50
+
+var slowed : bool = false
+
+@onready var collision_shape_2d = $CollisionShape2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	
+	velocity.x = speed
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_force
+	
+	collision_shape_2d.shape.radius = 10
+	if Input.is_action_pressed("crouch"):
+		velocity.y = velocity.y + crouch_force * delta
+		collision_shape_2d.shape.height = 20
+	else:
+		collision_shape_2d.shape.height = 30
+	
+	move_and_slide()
