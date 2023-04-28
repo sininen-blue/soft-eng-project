67a82f815438e8cc78c50ca1800b56c2179443b9
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2023-04-28 14:51:32 +0800

    added animation handler for player
    also defined some settings

--- characters/player.gd
+++ characters/player.gd
@@ -25,6 +25,7 @@ var gravity = 400
 var slowed : bool = false
 
 @onready var collision_shape_2d = $CollisionShape2D
+@onready var animation_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -33,6 +34,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	handle_animations()
+	
 	velocity.y += gravity * delta
 	
 	velocity.x = speed
@@ -40,11 +43,18 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_force
 	
-	collision_shape_2d.shape.radius = 10
+	collision_shape_2d.shape.radius = 7
 	if Input.is_action_pressed("crouch"):
 		velocity.y = velocity.y + crouch_force * delta
 		collision_shape_2d.shape.height = 15
 	else:
-		collision_shape_2d.shape.height = 30
+		collision_shape_2d.shape.height = 18
 	
 	move_and_slide()
+
+
+func handle_animations():
+	if !is_on_floor():
+		animation_player.play("jump")
+	else:
+		animation_player.play("run")
