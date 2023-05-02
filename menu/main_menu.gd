f4e11ab2b4f532b4b3da90a557ec1d513998c080
sininen-blue <108949710+sininen-blue@users.noreply.github.com>
2023-05-02 20:55:34 +0800

    main menu

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -1,5 +1,11 @@
 extends Control
 
+
+@onready var animation_player = $AnimationPlayer
+
+func _ready():
+	animation_player.play("sway")
+
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://levels/test_level.tscn")
 
